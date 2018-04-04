d57f751fcde4b5116917c05bdcd5a10785bd6536
shiro <shiro.mns@gmail.com>
2018-04-04 18:35:57 +0900

    [修正]過去結果一括解析のファイル名

--- _execue_all.sh
+++ _execue_all.sh
@@ -4,16 +4,16 @@ CURENT=`pwd`	#実行ディレクトリの保存
 cd `dirname $0`	#解析コードのあるディレクトリで作業をする
 
 for ((RESULT_NO=$1;RESULT_NO <= $2;RESULT_NO++)) {
-    for ((GENERATE_NO=0;GENERATE_NO >=0;GENERATE_NO--)) {
-        RESULT_NO0=`printf "%02d" $RESULT_NO`
+    for ((GENERATE_NO=5;GENERATE_NO >=0;GENERATE_NO--)) {
+        RESULT_NO0=`printf "%d" $RESULT_NO`
         
         ZIP_NAME=${RESULT_NO0}_$GENERATE_NO
 
-        #if [ -f ./data/utf/turn${ZIP_NAME}.zip ]; then
+        if [ -f ./data/utf/turn${ZIP_NAME}.zip ]; then
             echo "start $ZIP_NAME"
             ./execute.sh $RESULT_NO $GENERATE_NO
             break
-        #fi
+        fi
     }
 }
 
